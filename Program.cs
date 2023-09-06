c269b41e68efedb4493908fdcafc0ffda00ae4f1
PavanS1609 <142468149+PavanS1609@users.noreply.github.com>
2023-09-06 19:21:01 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+//Fibonacci.Fibonacci_Series();
+//Perfect_Number.PerfectNumber();
+//ReverseNumber.Reverse();
+PrimeNumber.Prime();
