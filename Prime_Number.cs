da9f539e68d7875fdee3bf80b7c9a6720431d6bf
PavanS1609 <142468149+PavanS1609@users.noreply.github.com>
2023-09-06 19:17:04 +0530

    Add files via upload

---
+++ Prime_Number.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+class PrimeNumber
+{
+    static bool IsPrime(int num)
+    {
+        if (num <= 1)
+            return false;
+
+        if (num <= 3)
+            return true;
+
+        if (num % 2 == 0 || num % 3 == 0)
+            return false;
+
+        for (int i = 5; i * i <= num; i += 6)
+        {
+            if (num % i == 0 || num % (i + 2) == 0)
+                return false;
+        }
+
+        return true;
+    }
+
+    public static void Prime()
+    {
+        Console.Write("Enter a number to check if it's a prime number: ");
+        int number = int.Parse(Console.ReadLine());
+
+        if (IsPrime(number))
+        {
+            Console.WriteLine(number + " is a prime number.");
+        }
+        else
+        {
+            Console.WriteLine(number + " is not a prime number.");
+        }
+    }
+}
+
