3064a56ec81ee5ad0d7f34e035dbe22cbe9843c1
PavanS1609 <142468149+PavanS1609@users.noreply.github.com>
2023-09-06 19:19:15 +0530

    Add files via upload

---
+++ Reverse_Number.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+class ReverseNumber
+{
+    static int Reverse(int num)
+    {
+        int reversedNum = 0;
+
+        while (num != 0)
+        {
+            int digit = num % 10;
+            reversedNum = reversedNum * 10 + digit;
+            num /= 10;
+        }
+
+        return reversedNum;
+    }
+    public static void Reverse()
+    {
+        Console.Write("Enter a number to reverse: ");
+        int number = int.Parse(Console.ReadLine());
+
+        int reversedNumber = Reverse(number);
+
+        Console.WriteLine("Reversed number: " + reversedNumber);
+    }
+}
+
