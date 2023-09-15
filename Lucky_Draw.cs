06e4b912e329ef871897009a13f315a59020f10d
PavanS1609 <142468149+PavanS1609@users.noreply.github.com>
2023-09-15 17:18:56 +0530

    Lucky_Draw

--- Lucky_Draw.cs
+++ Lucky_Draw.cs
@@ -1,11 +1,50 @@
-﻿using System;
+using System;
 
-class LuckyDraw
+using System;
+using System.Collections.Generic;
+class Lucky_Draw
 {
-    public static void Luckydraw()
-    {
-        Random random = new Random();
-        int num=random.Next(0,6);
+       
+        static int GenerateRandomCoupon(int maxCoupon)
+        {
+            Random random = new Random();
+            return random.Next(1, maxCoupon + 1);
+        }
 
-    }
+       
+        public static int GetTotalRandomNumbers(int n)
+        {
+            HashSet<int> distinctCoupons = new HashSet<int>();
+            int totalRandomNumbers = 0;
+
+            while (distinctCoupons.Count < n)
+            {
+                int randomCoupon = GenerateRandomCoupon(n);
+                totalRandomNumbers++;
+
+                if (!distinctCoupons.Contains(randomCoupon))
+                {
+                    distinctCoupons.Add(randomCoupon);
+                }
+            }
+
+            return totalRandomNumbers;
+        }
+    
+
+   
+     public static void lucky_draw()
+     { 
+            Console.WriteLine("Enter the number of distinct coupon numbers (N): ");
+            if (int.TryParse(Console.ReadLine(), out int n) && n > 0)
+            {
+                int totalRandomNumbers = Lucky_Draw.GetTotalRandomNumbers(n);
+                Console.WriteLine($"Total random numbers needed to get all distinct coupons: {totalRandomNumbers}");
+            }
+            else
+            {
+                Console.WriteLine("Please enter a valid positive integer for N.");
+            }
+     }
 }
+
