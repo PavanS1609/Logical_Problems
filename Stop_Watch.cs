0fe2c48dffe5283dd2a81c07ccab030fe2fa0e3c
PavanS1609 <142468149+PavanS1609@users.noreply.github.com>
2023-09-15 17:15:41 +0530

    Stop_Watch

--- Stop_Watch.cs
+++ Stop_Watch.cs
@@ -1,8 +1,45 @@
-﻿using System;
-class StopWatch
-{
-    public static void Stop_Watch()
-    {
+using System;
+using System;
 
-    }
-}
+ class Stop_Watch
+ {
+      public  static void stop_Watch() 
+      {
+            Console.WriteLine("Stopwatch Program");
+
+            while (true)
+            {
+                Console.WriteLine("Press 'S' to start the stopwatch or 'Q' to quit: ");
+                var choice = Console.ReadKey(intercept: true).Key;
+
+                if (choice == ConsoleKey.S)
+                {
+                    double elapsedMilliseconds = MeasureElapsedTime();
+                    Console.WriteLine($"Elapsed Time: {elapsedMilliseconds / 1000:F2} seconds");
+                }
+                else if (choice == ConsoleKey.Q)
+                {
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Invalid choice. Please enter 'S' to start or 'Q' to quit.");
+                }
+            }
+        }
+
+        static double MeasureElapsedTime()
+        {
+            Console.WriteLine("Press Enter to start the stopwatch...");
+            Console.ReadLine(); // Wait for Enter key
+            DateTime startTime = DateTime.Now;
+
+            Console.WriteLine("Press Enter again to stop the stopwatch...");
+            Console.ReadLine(); // Wait for Enter key
+            DateTime endTime = DateTime.Now;
+
+            TimeSpan elapsed = endTime - startTime;
+            return elapsed.TotalMilliseconds;
+        }
+ }
+ 
